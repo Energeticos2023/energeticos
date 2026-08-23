d45e3f62945f5d44238f459a107a781870972963
Club Rotary Huaraz Colonial <jose.zena.p@uni.pe>
2026-08-23 09:18:31 -0500

    Add files via upload

--- zena-industrial/sw.js
+++ zena-industrial/sw.js
@@ -1,5 +1,10 @@
-const CACHE='zena-industrial-v3';
-const ASSETS=['./','index.html','manifest.webmanifest'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+const CACHE='zena-industrial-v4';
+const ASSETS=['./','manifest.webmanifest','favicon.svg','icon-192.png','icon-512.png',
+'assets/01-planta-minera.jpg','assets/03-montaje-estructura.jpg','assets/04-reparacion-maquinaria.jpg','assets/05-soldadura-excavadora.jpg'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>Promise.allSettled(ASSETS.map(a=>c.add(a)))))});
 self.addEventListener('activate',e=>{e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()]))});
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./'))))});
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET')return;
+  if(new URL(e.request.url).origin!==location.origin)return;
+  e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./'))));
+});

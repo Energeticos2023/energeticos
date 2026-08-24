a4b872200fceeac5a87c0705e6f612f85b69dfb7
Club Rotary Huaraz Colonial <jose.zena.p@uni.pe>
2026-08-24 10:02:28 -0500

    Add files via upload

--- zena-industrial/sw.js
+++ zena-industrial/sw.js
@@ -1,9 +1,10 @@
-const CACHE='zena-industrial-v5';
-const ASSETS=['./','manifest.webmanifest','favicon.svg','icon-192.png','icon-512.png'];
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>Promise.allSettled(ASSETS.map(a=>c.add(a)))))});
-self.addEventListener('activate',e=>{e.waitUntil(Promise.all([caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))),self.clients.claim()]))});
+const CACHE='zena-industrial-v6';
+const CORE=['./','manifest.webmanifest','favicon.svg','icon-192.png','icon-512.png',
+'assets/soldadura-cucharon-1400.webp','assets/logo-energeticos.png'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>Promise.allSettled(CORE.map(a=>c.add(a)))))});
+self.addEventListener('activate',e=>{e.waitUntil(Promise.all([caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))),self.clients.claim()]))});
 self.addEventListener('fetch',e=>{
   if(e.request.method!=='GET')return;
   if(new URL(e.request.url).origin!==location.origin)return;
-  e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./'))));
+  e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./'))));
 });
